a3cc8a189862163d27c3574f09ce26756db2ad7c
Dulguun <dulguun.web@gmail.com>
2022-11-30 20:56:47 +0800

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getServerSideProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  var cnt = 0;
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <div>
+          {allPostsData.map((post) => {
+            cnt++;
+            if (cnt < 4) {
+              return (
+                <div key={post.id}>
+                  <p>{post.title}</p>
+                  <p>{cnt}</p>
+                </div>
+              );
+            }
+          })}
+        </div>
+
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
