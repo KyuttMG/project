f1f990467b6eb17ea8fdacb90e0a5f1e20f39b01
Dulguun <dulguun.web@gmail.com>
2022-12-08 00:13:18 +0800

    commit

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,8 +1,18 @@
+import utilStyles from "../../styles/utils.module.css";
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
@@ -12,6 +22,20 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
-export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
