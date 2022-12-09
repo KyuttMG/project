6068f3b6e481da681d917719f45001a577956235
miigaa <galowog@gmail.com>
2022-12-09 08:55:46 +0800

    miiga commit

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Assassin_MGL";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "MG in da webhouse";
 
 export default function Layout({ children, home }) {
   return (
